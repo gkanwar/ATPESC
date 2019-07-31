cc20acefd1de38b3a92c640643c53d194012d9f5
Gurtej Kanwar <gurtej@mit.edu>
2019-07-31 16:18:23 -0500

    Adds race condition to racey

--- OMP_Exercises/racey.c
+++ OMP_Exercises/racey.c
@@ -1,15 +1,26 @@
-
 #include <stdio.h>
 #include <omp.h>
+#include <stdlib.h>
 
 int main()
 {
 
   printf("I think");
 
-  printf(" car");
-
-  printf(" race");
+  #pragma omp parallel
+  {
+    #pragma omp single
+    {
+      #pragma omp task
+      {
+        /* annoying trick to get seeming randomness */
+        for (int i = 0; i < rand() % 100; i++) {}
+        printf(" car");
+      }
+      #pragma omp task
+      printf(" race");
+    }
+  }
 
   printf("s are fun\n");
 
