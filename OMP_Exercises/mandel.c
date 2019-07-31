1e0463f3e157a63c1e3a944a211028a1073862b6
Gurtej Kanwar <gurtej@mit.edu>
2019-07-31 14:06:16 -0500

    Fixes mandel implementation, optimized with dynamic loop schedule

--- OMP_Exercises/mandel.c
+++ OMP_Exercises/mandel.c
@@ -17,65 +17,67 @@
 #include <math.h>
 #include <omp.h>
 
-# define NPOINTS 1000
+# define NPOINTS 5000
 # define MAXITER 1000
 
-void testpoint(void);
-
 struct d_complex{
-   double r;
-   double i;
+  double r;
+  double i;
 };
 
-struct d_complex c;
-int numoutside = 0;
+int testpoint(struct d_complex);
+
 
 int main(){
-   int i, j;
-   double area, error, eps  = 1.0e-5;
+  int i, j;
+  double area, error, eps = 1.0e-5;
+  int numoutside = 0;
 
+  //   Loop over grid of points in the complex plane which contains the Mandelbrot set,
+  //   testing each point to see whether it is inside or outside the set.
 
-//   Loop over grid of points in the complex plane which contains the Mandelbrot set,
-//   testing each point to see whether it is inside or outside the set.
+  double start_time = omp_get_wtime();
 
-#pragma omp parallel for default(shared) private(c,eps)
-   for (i=0; i<NPOINTS; i++) {
-     for (j=0; j<NPOINTS; j++) {
-       c.r = -2.0+2.5*(double)(i)/(double)(NPOINTS)+eps;
-       c.i = 1.125*(double)(j)/(double)(NPOINTS)+eps;
-       testpoint();
-     }
-   }
+  #pragma omp parallel for schedule(dynamic, NPOINTS/100) default(none) shared(eps) private(j) reduction(+:numoutside)
+  for (i=0; i<NPOINTS; i++) {
+    for (j=0; j<NPOINTS; j++) {
+      struct d_complex c;
+      c.r = -2.0+2.5*(double)(i)/(double)(NPOINTS)+eps;
+      c.i = 1.125*(double)(j)/(double)(NPOINTS)+eps;
+      numoutside += testpoint(c);
+    }
+  }
 
-// Calculate area of set and error estimate and output the results
+  // Calculate area of set and error estimate and output the results
    
-area=2.0*2.5*1.125*(double)(NPOINTS*NPOINTS-numoutside)/(double)(NPOINTS*NPOINTS);
-   error=area/(double)NPOINTS;
+  area=2.0*2.5*1.125*(double)(NPOINTS*NPOINTS-numoutside)/(double)(NPOINTS*NPOINTS);
+  error=area/(double)NPOINTS;
 
-   printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n",area,error);
-   printf("Correct answer should be around 1.510659\n");
+  double run_time = omp_get_wtime() - start_time;
 
+  printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n",area,error);
+  printf("Correct answer should be around 1.510659\n");
+  printf("Took %lf seconds\n", run_time);
 }
 
-void testpoint(void){
+int testpoint(struct d_complex c){
 
-// Does the iteration z=z*z+c, until |z| > 2 when point is known to be outside set
-// If loop count reaches MAXITER, point is considered to be inside the set
+  // Does the iteration z=z*z+c, until |z| > 2 when point is known to be outside set
+  // If loop count reaches MAXITER, point is considered to be inside the set
 
-       struct d_complex z;
-       int iter;
-       double temp;
+  struct d_complex z;
+  int iter;
+  double temp;
 
-       z=c;
-       for (iter=0; iter<MAXITER; iter++){
-         temp = (z.r*z.r)-(z.i*z.i)+c.r;
-         z.i = z.r*z.i*2+c.i;
-         z.r = temp;
-         if ((z.r*z.r+z.i*z.i)>4.0) {
-           numoutside++;
-           break;
-         }
-       }
-
+  z=c;
+  for (iter=0; iter<MAXITER; iter++){
+    temp = (z.r*z.r)-(z.i*z.i)+c.r;
+    z.i = z.r*z.i*2+c.i;
+    z.r = temp;
+    if ((z.r*z.r+z.i*z.i)>4.0) {
+      return 1;
+    }
+  }
+  return 0;
 }
 
